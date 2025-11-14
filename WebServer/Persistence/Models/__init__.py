82adfd6bddbd75a1878c39f7224947444cd630ee
Juan Churio <juan.churio.dr@gmail.com>
2025-11-14 09:52:25 -0500

    ci passing

--- WebServer/Persistence/Models/__init__.py
+++ WebServer/Persistence/Models/__init__.py
@@ -7,9 +7,9 @@ from .Report import RATE as REPORT_RATE
 from .Report import Report as Report
 from .Subscription import TIER as SUBSCRIPTION_TIER
 from .Subscription import Subscription as Subscription
+from .Token import Tokens as Tokens
 from .User import Role as USER_ROLE
 from .User import User as User
 from .UserLanguage import PROFICIENCY_LEVELS as USER_PROFICIENCY_LEVELS
 from .UserLanguage import UserLanguage as UserLanguage
 from .UserMessage import UserMessage
-from .Token import Tokens as Tokens
